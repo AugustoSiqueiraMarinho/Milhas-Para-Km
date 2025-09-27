9e409cfa47f67eac0a40070c2608debd7a4e8c2d
Augusto S <augustosiqueira000@etec.sp.gov.br>
2025-09-27 01:26:03 -0300

    Exercício Finalizado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("--- Milhas para KM ---");
+Console.Write("Informe o valor em milhas: ");
+//1 - Declarando a string, atribuindo e convertendo o double
+string entrada = Console.ReadLine()!;
+double milhas = Convert.ToDouble(entrada);
+
+//2 - Declarando e convertendo diretamente o double (opção ao 1)
+//double _milhas + Convert.ToDouble(Console.ReadLine()!);
+
+double resultado = milhas * 1.609;
+
+Console.WriteLine($"{milhas} milhas em km --> {resultado}");
